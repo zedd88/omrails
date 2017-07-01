8f3797e1055a49706776fe59b1d446cd5a9a14b0
zedd88 <saquib.anwar@icloud.com>
2017-07-01 13:16:45 +1200

    Add users with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  
+  devise_for :users
+
+
+
+devise_scope :user do
+    # write all your routes inside this block
+    get 'signin' => 'devise/sessions#new'
+  delete 'signout' => 'devise/sessions#destroy'
+  get 'signup' => 'devise/registrations#new'
+end
+
+
+
   root 'pages#home'
   get '/' => 'pages#home'
   get 'about' => 'pages#about'
