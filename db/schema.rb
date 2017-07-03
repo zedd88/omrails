f0590971c25515ed5425cf9434a55a636188230e
zedd88 <saquib.anwar@icloud.com>
2017-07-03 17:16:32 +1200

    Added Active Admin Authorisation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170701021910) do
+ActiveRecord::Schema.define(version: 20170703034130) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20170701021910) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
